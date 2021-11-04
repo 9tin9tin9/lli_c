ccd1d498237bacb9c7caac86344cc096187e32e4
9tin9tin9 <9tin9tin9@gmail.com>
2021-11-04 18:44:05 +0800

    Error_print prints error name

--- src/error.c
+++ src/error.c
@@ -4,6 +4,11 @@
 
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
+#define error_case(e_) \
+    case e_: \
+        eprintf(#e_ "\n"); \
+        break;
+
 void
 Error_print(Error r, void* _c, int level)
 {
@@ -11,6 +16,34 @@ Error_print(Error r, void* _c, int level)
     Code* c = _c;
 
     switch (r) {
+        error_case(Error_WrongTokTypeForOp);
 
+        error_case(Error_ParseNumError);
+        error_case(Error_ParseIdxError);
+        error_case(Error_UnterminatedIdx);
+        error_case(Error_EmptyIdx);
+        error_case(Error_MissingVarName);
+
+        error_case(Error_EmptyToken);
+        error_case(Error_UnexpectedDelim);
+        error_case(Error_NonDelimAfterSymEnd);
+        error_case(Error_DoubleQuoteInMiddle);
+        error_case(Error_UnknownEscapeSequence);
+
+        error_case(Error_UndefinedVar);
+        error_case(Error_UndefinedLabel);
+        error_case(Error_UnknownOp);
+
+        error_case(Error_InvalidMemAccess);
+        error_case(Error_CannotWriteToNMem);
+        error_case(Error_NotInteger);
+        error_case(Error_NotPositiveInteger);
+        error_case(Error_WrongArgType);
+        error_case(Error_WrongArgCount);
+        error_case(Error_CannotOpenFile);
+        error_case(Error_CannotReadFile);
+
+        default:
+            break;
     }
 }
